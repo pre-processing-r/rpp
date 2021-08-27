801a575b5b784d76654d9abb2181992888dc5901
Kirill Müller <krlmlr+r@mailbox.org>
2021-08-27 05:01:02 +0200

    declare

--- R/inline_plugin.R
+++ R/inline_plugin.R
@@ -1,5 +1,5 @@
 #' @export
-declare_inline_plugin <- function(dev, prod) {
+inline_plugin <- function(dev, prod) {
   structure(
     list(dev = dev, prod = prod),
     class = c("rpp_inline_plugin", "rpp_plugin")
